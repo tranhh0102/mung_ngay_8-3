58c379a6587a92fe783695606d0953e120b960f9
tranhh0102 <91929935+tranhh0102@users.noreply.github.com>
2022-03-04 17:34:15 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Chị Em HC';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Ngày 8/3';
+const titleCard = 'Tặng tất cả các chị em HC';
+const contentCard = 'Nhân ngày Quốc tế Phụ nữ 8/3, Tranh chúc mng yêu thật nhiều sức khỏe, luôn tươi cười và cầu chúc mọi điều tốt lành nhất đến với mng.';
+
+const giftImage = 'thiep.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
